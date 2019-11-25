04ea70dc121926c9e711e73ba4d1a71d31135d5a
Pavel Bozdog <pavel@scanwellhealth.com>
2019-11-25 15:17:41 -0800

    Set up new Xcode project

---
+++ Camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Camera
+//
+//  Created by Pavel Bozdog on 11/25/19.
+//  Copyright © 2019 Pavel Bozdog. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
